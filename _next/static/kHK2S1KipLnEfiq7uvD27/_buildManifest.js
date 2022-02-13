6b97f42adfbb752a9978662e3686a8b831cce448
ivnbogdan <bogdan.ivanov90@gmail.com>
2022-02-13 20:24:55 +0000

    Deploying to gh-pages from @ ninjastacktech/react-nft-wallet@d211aeaa16acc44ed0ce8ea5cd2b299d923cb855 🚀

---
+++ _next/static/kHK2S1KipLnEfiq7uvD27/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f9f66dc5f41fcf1c.css","static/chunks/pages/index-f3149b5f4d5fc014.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
